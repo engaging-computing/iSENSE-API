efd5b6d22bfca3f7315e9dbbdd3b723b0d926c5b
Tyler <tyler.puleo22@gmail.com>
2014-09-23 17:09:10 -0400

    removed comments

--- Python/GetandPost.py
+++ Python/GetandPost.py
@@ -3,7 +3,6 @@ import json
 import urllib2
 import requests
 
-
 project = raw_input('Enter a project id: ')
 
 title = raw_input('Enter a project title: ')
@@ -13,8 +12,6 @@ email = raw_input('Enter email: ')
 password = raw_input('Enter password: ')
 
 data = json.load(urllib2.urlopen('http://rsense-dev.cs.uml.edu/api/v1/projects/'+project))
-
-
 
 # Data stuff
 email = ''+ email
@@ -39,46 +36,3 @@ headers = {'content-type': 'application/json'}
 r = requests.post(url, data=json.dumps(payload), headers=headers)
 r.raise_for_status()
 r.status_code
-
-
-
-
-
-
-
-
-"""
-import requests, json,urllib2
-
-
-r = []
-r = requests.get("http://rsense-dev.cs.uml.edu/api/v1/projects")
-
-#data = json.load(urllib2.urlopen('http://rsense-dev.cs.uml.edu/api/v1/projects/683'))
-
-##print data
-
-# Data stuff
-email = 'j@j.j'
-password = 'j'
-title = 'Hello World. WOO HOO'
-
-# POST stuff.
-url = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683/jsonDataUpload'
-
-payload = {
-    'email': email,
-    'password': password,
-    'title': title,
-    'data':
-    {
-    	'3129': [20]
-    }
-}
-
-headers = {'content-type': 'application/json'}
-
-r = requests.post(url, data=json.dumps(payload), headers=headers)
-r.raise_for_status()
-r.status_code
-"""
