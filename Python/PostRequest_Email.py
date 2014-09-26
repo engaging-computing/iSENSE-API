15e7ac4266f9029c84065e81be959379e036255e
Jason <jason_downing@student.uml.edu>
2014-09-26 16:00:02 -0400

    added more checks to the POST Email/Password example script

--- Python/PostRequest_Email.py
+++ Python/PostRequest_Email.py
@@ -1,4 +1,4 @@
-import requests, json, getpass  
+import requests, json, getpass
 
 # Requests will probably need to be installed.
 # Try pip install requests
@@ -8,10 +8,23 @@ import requests, json, getpass
 print "Basic iSENSE JSON uploader using email/password\n";
 
 email = raw_input("Enter an email: ")
-password = getpass.getpass("Enter your password: ")     # These characters are hidden from the user.
+password = getpass.getpass("Enter your password: ")     # These chars are hidden from the user.
 title = raw_input("Enter the title of the dataset: ")
 data = raw_input("Enter a number: ")
 choice = raw_input("Do you want to upload to iSENSE? (Y/N) -> ")
+
+# Do a GET request to see if the user entered the correct email/password.
+r = requests.get("http://rsense-dev.cs.uml.edu/api/v1/users/myInfo?email="+email+'&password='+password)
+
+# See if the user's email/password is valid. Loop until they enter the correct credentials.
+while r.status_code != 200:
+    print"\n\n**************************************************************************"
+    print "\tThe email and/or password you entered was incorrect."
+    print"**************************************************************************\n"
+    email = raw_input("Enter an email: ")
+    password = getpass.getpass("Enter your password: ")     # These chars are hidden from the user.
+
+    r = requests.get("http://rsense-dev.cs.uml.edu/api/v1/users/myInfo?email="+email+'&password='+password)
 
 # User typed "N" or some other random character. So they didn't want to POST.
 if choice == 'N':
@@ -21,7 +34,7 @@ if choice == 'N':
 print "\nUPLOADING TO iSENSE."
 
 # POST stuff.
-url = 'http://rsense-dev.cs.uml.edu/api/v1/projects/744/jsonDataUpload'
+POST_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/744/jsonDataUpload'
 
 payload = {
     'email': email,
@@ -36,16 +49,20 @@ payload = {
 # This is needed, otherwise I got code 422s and the data wasn't showing up right in the log.
 headers = {'content-type': 'application/json'}
 
-r = requests.post(url, data=json.dumps(payload), headers=headers)
+r = requests.post(POST_URL, data=json.dumps(payload), headers=headers)
 
 # Detects status codes and tells the user what went right or wrong.
 if r.status_code == 200:
     print "\nUpload completed successfully!"
 if r.status_code == 401:
-    print "\nHmm, got an error code of 401."
+    print "\nError code: 401."
     print "Try entering  the correct email and password combination."
 if r.status_code == 422:
-    print "\nGot an error code of 422."
+    print "\nError code: 422."
     print "Try entering all the data fields correctly, with the right type for each field."
-    print "IE a number must be a number, a string must be a series of characters,"
-    print "Lat/Long should be GPS coordinates and a timestamp should be correctly formatted."
+    print "Examples: "
+    print "Numbers must be numbers: 1, 2, 5.555435, etc"
+    print "Strings must be a series of characters: \'hello world\', \'this is a string\', etc."
+    print "Latitude and Longitude must be degrees:  42.65 -71.32, 33.83 -118,  etc."
+    print "Timestamps must be in the form \"yyyy/MM/dd hh:mm:ss\" or as a UNIX timestamp."
+    print "\n\nTry rerunning this program, making sure to follow the above format restrictions."
