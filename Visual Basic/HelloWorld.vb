f310e08ad89388a23e70414b58359bf97dc4ebb4
Jason <jason_downing@student.uml.edu>
2014-10-01 16:38:15 -0400

    VB
    Correct file this time.

--- Visual Basic/HelloWorld.vb
+++ Visual Basic/HelloWorld.vb
@@ -1,3 +1,8 @@
+' This is a Hello, World Example program in Visual Basic
+' All it does is make a box pop up saying "Hello, World!
+' See the following link for more information about programming in Visual Basic:
+' http://msdn.microsoft.com/en-us/library/y4wf33f0(v=vs.110).aspx
+
 ' A "Hello, World!" program in Visual Basic. 
 Module Hello
     Sub Main()
