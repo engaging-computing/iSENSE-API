3635313c1bb69ec66781ba952c38882fe113b707
Jason <jason_downing@student.uml.edu>
2014-10-15 16:28:51 -0400

    got POST cleaned up and moved into a function. Basic diceapp now working. Going to update it to include more simulations and probably upload 100 random dice throws vs just 1.

--- C++/GET/GET_basic_curl.cpp
+++ C++/GET/GET_basic_curl.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
-#include <curl/curl.h>      //CURL to make HTTP requests
 #include <stdio.h>
+#include <curl/curl.h>      //CURL to make HTTP requests
 
 using namespace std;
 
 int main()
 {
     /*
-        C++ libraries to try
-        boost.asio
-        curl
-        win socks /
-    */
-
-        /*
-            How to get CURL working on Ubuntu 14.04LTS
-
-            sudo apt-get install curl
-            sudo apt-get install libcurl4-gnutls-dev
-            sudo apt-get update
-
-            See this site for help:
-            http://curl.haxx.se/docs/httpscripting.html
-
-            WHEN COMPILING A WITH CURL, MAKE SURE TO INCLUDE "-lcurl" at the end of the gcc/g++ command.
-            Example:
-            g++ GET_basic_curl.cpp -lcurl
-
-            Also: wow, the following actual works! It grabs the project page for project #744 
+            The following actual works! It grabs the project page for project #744
             and displays it to the terminal!
+
             And the following is straight off of the cURL webpage. See:
             http://curl.haxx.se/libcurl/c/example.html
-        */
+    */
 
     // This project will try using CURL to make a basic GET request to rSENSE
         CURL *curl;
@@ -56,7 +37,7 @@ int main()
         curl_easy_cleanup(curl);
     }
 
-    cout<<"\n\n";
+    cout<<"\n\n";       // Add some extra lines on the terminal.
 
     return 0;
 }
--- C++/README.md
+++ C++/README.md
@@ -1,1 +1,43 @@
 This folder will contain the C++ Example Code.
+
+Some of these examples use cURL through the libcurl library for c/c++.
+
+You can get libcurl on Linux (Ubuntu/Debian) by running the following commands:
+
+sudo apt-get install curl
+
+sudo apt-get install libcurl4-gnutls-dev
+
+sudo apt-get update
+
+(I will include Windows instructions after I try compiling with Visual Studios Express.)
+
+
+See this site for help:
+
+http://curl.haxx.se/docs/httpscripting.html
+
+
+NOTE:
+
+When compiling with curl, be sure to include "-lcurl" at the end of the gcc/g++ command.
+
+Example:
+
+g++ GET_basic_curl.cpp -lcurl
+
+
+Other  C++ libraries to try:
+
+boost/asio
+
+win socks / Microsoft's REST SDK for C++
+
+
+Currently Working:
+
+GET with libcurl
+
+POST with libcurl
+
+Future: DiceApp for C++.
