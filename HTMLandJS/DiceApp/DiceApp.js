b328169af05befd8c520f061862fe357f6210f7c
Tyler Puleo <tyler.puleo22@gmail.com>
2014-10-29 15:18:56 -0400

    Changed tabs to spaces

--- HTMLandJS/DiceApp/DiceApp.js
+++ HTMLandJS/DiceApp/DiceApp.js
@@ -2,14 +2,14 @@ var dice_roll_array = [];
  
 function Roll_Di(){
 
-	dice_roll_array.push(Math.floor(Math.random() * 6) + 1);
-	$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
+    dice_roll_array.push(Math.floor(Math.random() * 6) + 1);
+    $("#Printout").text("Dice Rolls: " + dice_roll_array.join());
 }
 
 function Post_Di_Array(){
 
-	var currentTime = new Date();
-	var timestamp = JSON.stringify(currentTime);
+    var currentTime = new Date();
+    var timestamp = JSON.stringify(currentTime);
     var API_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/927/jsonDataUpload';
     var upload = {
         'title': 'Test' + timestamp,
@@ -17,33 +17,33 @@ function Post_Di_Array(){
         'contributor_name': 'Tyler',
         'data':
         {
-        	'4251': dice_roll_array
+            '4251': dice_roll_array
         }
     }
 
     if(dice_roll_array[0] == null){
-    	alert("There are no dice rolls. Please roll di before posting");
+        alert("There are no dice rolls. Please roll di before posting");
     }
     else{
 
-	    $.post(API_URL, upload);
+        $.post(API_URL, upload);
 
-	    dice_roll_array = [];
-		$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
-	    alert("Rolls have been Posted");
-	}
+        dice_roll_array = [];
+        $("#Printout").text("Dice Rolls: " + dice_roll_array.join());
+        alert("Rolls have been Posted");
+    }
 }
 
 function Post_1000_Di_Rolls(){
-	
-	var currentTime = new Date();
-	var timestamp = JSON.stringify(currentTime);
-	var i;
+    
+    var currentTime = new Date();
+    var timestamp = JSON.stringify(currentTime);
+    var i;
 
-	dice_roll_array = [];
+    dice_roll_array = [];
 
     for(i = 0; i<1000; i++){
-    	Roll_Di();
+        Roll_Di();
     }
 
     Post_Di_Array();
@@ -51,6 +51,6 @@ function Post_1000_Di_Rolls(){
 
 function Clear_Dice_Rolls(){
 
-	dice_roll_array = [];
-	$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
+    dice_roll_array = [];
+    $("#Printout").text("Dice Rolls: " + dice_roll_array.join());
 }
