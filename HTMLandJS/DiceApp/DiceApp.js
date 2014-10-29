711550d0ba5fa2b0b3e5fcb91dc1fcc727679b41
Tyler Puleo <tyler.puleo22@gmail.com>
2014-10-29 15:17:38 -0400

    Dice rolls now appearing on screen instead of popup. Error checking in post

--- HTMLandJS/DiceApp/DiceApp.js
+++ HTMLandJS/DiceApp/DiceApp.js
@@ -3,15 +3,11 @@ var dice_roll_array = [];
 function Roll_Di(){
 
 	dice_roll_array.push(Math.floor(Math.random() * 6) + 1);
-
-}
-
-function Show_Di_Array(){
-
-	alert(dice_roll_array);
+	$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
 }
 
 function Post_Di_Array(){
+
 	var currentTime = new Date();
 	var timestamp = JSON.stringify(currentTime);
     var API_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/927/jsonDataUpload';
@@ -24,37 +20,37 @@ function Post_Di_Array(){
         	'4251': dice_roll_array
         }
     }
-    $.post(API_URL, upload);
 
-    alert("Rolls have been Posted");
-    dice_roll_array = [];
+    if(dice_roll_array[0] == null){
+    	alert("There are no dice rolls. Please roll di before posting");
+    }
+    else{
+
+	    $.post(API_URL, upload);
+
+	    dice_roll_array = [];
+		$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
+	    alert("Rolls have been Posted");
+	}
 }
 
 function Post_1000_Di_Rolls(){
+	
 	var currentTime = new Date();
 	var timestamp = JSON.stringify(currentTime);
 	var i;
+
 	dice_roll_array = [];
+
     for(i = 0; i<1000; i++){
     	Roll_Di();
     }
 
-    var API_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/927/jsonDataUpload';
-    var upload = {
-        'title': 'TEST'+timestamp,
-        'contribution_key': 'Key',
-        'contributor_name': 'Tyler',
-        'data':
-        {
-        	'4251': dice_roll_array
-        }
-    }
-    $.post(API_URL, upload);
-    alert("Rolls have been Posted");
-    dice_roll_array = [];
-
+    Post_Di_Array();
 }
+
 function Clear_Dice_Rolls(){
 
 	dice_roll_array = [];
+	$("#Printout").text("Dice Rolls: " + dice_roll_array.join());
 }
