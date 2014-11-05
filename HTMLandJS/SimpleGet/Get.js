34b59cefa07f72ec08b90fa89732b95d99f55d79
Tyler <tyler.puleo22@gmail.com>
2014-11-05 13:56:14 -0500

    Made changes to simple get and post

--- HTMLandJS/SimpleGet/Get.js
+++ HTMLandJS/SimpleGet/Get.js
@@ -1,12 +1,14 @@
 
-URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683';
-response = $.ajax({ type: "GET",
-                url: URL,
-                async: false,
-                dataType: "JSON"
-                }).responseText;
-arg = JSON.parse(response);
+function GET(){
+	URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683';
+	response = $.ajax({ type: "GET",
+	                url: URL,
+	                async: false,
+	                dataType: "JSON"
+	                }).responseText;
+	arg = JSON.parse(response);
 
-console.log(arg); //prints to console entire response that is JSON parsed
+	//console.log(arg); //prints to console entire response that is JSON parsed
 
-console.log(arg.id); //prints to console specific field in this case id
+	console.log(arg.id); //prints to console specific field in this case id
+}
