f40c74cbf05df1cb45a49175062014f15fa5d93a
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-10 15:27:52 -0500

    Made variable name changes

--- Python/BaseballApp.py
+++ Python/BaseballApp.py
@@ -4,32 +4,32 @@ print '\n'
 
 timestamp = time.time()
 
-timestamp2 = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+timestamp_reformatted = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
-URL1 = "http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json"
+url_get = "http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json"
 
-r1 = []
-r1 = requests.get(URL1)
+response_get = []
+response_get = requests.get(url_get)
 
-status = r1.status_code
+status = response_get.status_code
 
 total_walks = []
 total_bases = []
 calculated_runs = []
 
 for i in range(0, 30):
-	total_walks.append(int(r1.json()['data'][i]['644']))
-	total_bases.append(int(r1.json()['data'][i]['647']))	
+	total_walks.append(int(response_get.json()['data'][i]['644']))
+	total_bases.append(int(response_get.json()['data'][i]['647']))	
 	calculated_runs.append((total_walks[i] + total_bases[i])/4.0)
 
 print "Walks: " , total_walks
 print "Total Bases: " , total_bases
 print "Calculated Runs: " , calculated_runs
 
-url2 = 'http://rsense-dev.cs.uml.edu/api/v1/projects/106/jsonDataUpload'
+url_post = 'http://rsense-dev.cs.uml.edu/api/v1/projects/106/jsonDataUpload'
 
 payload = {
-    'title': 'title '+timestamp2,           
+    'title': 'title '+timestamp_reformatted,           
     'contribution_key': 'key',    
     'contributor_name': 'Tyler',
     'data':
@@ -40,7 +40,7 @@ payload = {
 
 headers = {'content-type': 'application/json'}
 
-r2 = requests.post(url2, data=json.dumps(payload), headers=headers)
+response_post = requests.post(url_post, data=json.dumps(payload), headers=headers)
 
 print "Runs have been posted"
 
