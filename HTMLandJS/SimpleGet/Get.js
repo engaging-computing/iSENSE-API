a0d416efc4ec940f9ecd5b21080cda1826af42d5
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-10 15:20:42 -0500

    Fixed Simple GET to do what I needed it to for documentation

--- HTMLandJS/SimpleGet/Get.js
+++ HTMLandJS/SimpleGet/Get.js
@@ -1,12 +1,19 @@
+field_data_total_bases = [];
 
-URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683';
-response = $.ajax({ type: "GET",
-                url: URL,
-                async: false,
-                dataType: "JSON"
-                }).responseText;
-arg = JSON.parse(response);
+function GET(){
 
-console.log(arg); //prints to console entire response that is JSON parsed
+    URL = 'http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json';
+    response = $.ajax({ type: "GET",
+                        url: URL,
+                        async: false,
+                        dataType: "JSON"
+                    }).responseText;
 
-console.log(arg.id); //prints to console specific field in this case id
+    arg = JSON.parse(response);
+
+    for (var i=0;i<30;i++) {
+        field_data_total_bases.push(arg.data[i][647]) ;
+    }
+
+    console.log(field_data_total_bases);
+}
