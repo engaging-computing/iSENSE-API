c93121f293b1fe5565409219930f0399dddf590d
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-12 15:38:14 -0500

    Changed to contributor key

--- HTMLandJS/SimplePost/Post.js
+++ HTMLandJS/SimplePost/Post.js
@@ -1,19 +1,15 @@
-//This script is hard coded for project 556. To change project you must change the 556 in the url and add the appropriate field numbers.
-function submitter()
+function post()
 {
-    var API_URL = 'http://isenseproject.org/api/v1/projects/556/jsonDataUpload';
+    var API_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/106/jsonDataUpload';
     var formData = [];
     var upload = {
-        'email': 'j@j.j',
-        'password': 'j',
+
         'title': 'TEST',
+	    "contribution_key":"Key",
+	    "contributor_name":"Tyler",
         'data':
         {
-            '2685': [5],
-            '2640': [4],
-            '2641': [4],
-            '2642': [7],
-            '2643': [8]
+            '4279': [5]
         }
     }
         $.post(API_URL, upload);
