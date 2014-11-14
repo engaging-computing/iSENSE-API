9d0a1f4d1c3f0aceb8fe2597d15074f000ea7833
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-14 10:07:40 -0500

    Finished revising simple Post

--- HTMLandJS/SimplePost/Post.js
+++ HTMLandJS/SimplePost/Post.js
@@ -1,11 +1,11 @@
-function post()
-{
+function post(){
+    
     var url = 'http://rsense-dev.cs.uml.edu/api/v1/projects/106/jsonDataUpload';
     var upload = {
 
         'title': 'YOUR NAME HERE',
-	    "contribution_key":"Key",
-	    "contributor_name":"YOUR NAME HERE",
+        'contribution_key':'Key',
+        'contributor_name':'YOUR NAME HERE',
         'data':
         {
             '4279': [5]
