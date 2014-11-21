b54932bbc56ce9a588d676263edf682b0e1a559b
Jason <jason_downing@student.uml.edu>
2014-11-21 10:53:54 -0500

    fixed simpleget.js

--- HTML5/SimpleGet/Get.js
+++ HTML5/SimpleGet/Get.js
@@ -1,13 +1,3 @@
-URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683';
-response = $.ajax({ type: "GET",
-                url: URL,
-                async: false,
-                dataType: "JSON"
-                }).responseText;
-arg = JSON.parse(response);
-=======
-totalBases = [];
-
 function get(){
     URL = 'http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json';
     response = $.ajax({ type: "GET",
