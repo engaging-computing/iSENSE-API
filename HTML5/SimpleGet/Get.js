e0c81ec80778477759920ce715de6b078cd2a83d
Jason <jason_downing@student.uml.edu>
2014-11-21 10:49:34 -0500

    fixing html5 get

--- HTML5/SimpleGet/Get.js
+++ HTML5/SimpleGet/Get.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:ExampleCode/HTML5/SimpleGet/Get.js
 URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/683';
 response = $.ajax({ type: "GET",
                 url: URL,
@@ -10,8 +9,6 @@ arg = JSON.parse(response);
 totalBases = [];
 
 function get(){
->>>>>>> 6c7f8e691b43d72c3773d99c18f8907b91e2c5e4:ExampleCode/HTMLandJS/SimpleGet/Get.js
-
     URL = 'http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json';
     response = $.ajax({ type: "GET",
                         url: URL,
