a8c2068cb20decbf85dc02540d80cb21665140ae
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-24 14:14:17 -0500

    Made Mike's changes

--- HTMLandJS/BaseballStats/BaseballApp.js
+++ HTMLandJS/BaseballStats/BaseballApp.js
@@ -6,48 +6,49 @@ var actualRuns = [];
 function getDataSet() {
 
     if (fieldDataTotalBases[0] != null) { 
-        alert("Please clear before pulling new datasets")
-    } else {
+        alert("Please clear before pulling new datasets");
+        return;
 
-
-        URL = 'http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json';
-        response = $.ajax({ type: "GET",
-                            url: URL,
-                            async: false,
-                            dataType: "JSON"
-                        }).responseText;
-
-        parsedResponse = JSON.parse(response);
-
-        for (var i=0;i<30;i++) {
-            fieldDataTotalBases.push(parsedResponse.data[i][647]) ;
-            fieldDataWalks.push(parsedResponse.data[i][644]) ;
-            actualRuns.push(parsedResponse.data[i][638]) ;
-        }
-
-        $("#totalBases").text("Total Bases: " + fieldDataTotalBases.join());
-        $("#walks").text("Walks: " + fieldDataWalks.join());
-        $("#actualRuns").text("Actual Runs: " + actualRuns.join());
     }
+
+    URL = 'http://rsense-dev.cs.uml.edu/api/v1/data_sets/1190?recur=true.json';
+    response = $.ajax({ type: "GET",
+                        url: URL,
+                        async: false,
+                        dataType: "JSON"
+                    }).responseText;
+
+    parsedResponse = JSON.parse(response);
+
+    for (var i=0; i<30; i++) {
+        fieldDataTotalBases.push(parsedResponse.data[i][647]) ;
+        fieldDataWalks.push(parsedResponse.data[i][644]) ;
+        actualRuns.push(parsedResponse.data[i][638]) ;
+    }
+
+    $("#totalBases").text("Total Bases: " + fieldDataTotalBases.join());
+    $("#walks").text("Walks: " + fieldDataWalks.join());
+    $("#actualRuns").text("Actual Runs: " + actualRuns.join());   
 }
 
 function calculateRuns() {
 
     if (calculatedRuns[0] != null) {
-        alert("Please clear before recalculating")
-    } else {
-
-        if (fieldDataWalks[0] == null) {
-            alert("Please pull datasets before calculating");
-        } else {
-
-            for (var i=0;i<30;i++) {
-                calculatedRuns.push((Number(fieldDataWalks[i]) + Number(fieldDataTotalBases[i])) /4.0);
-            }
-
-            $("#calculatedRuns").text("Calculated Runs: " + calculatedRuns.join());
-        }
+        alert("Please clear before recalculating");
+        return;
     }
+
+    if (fieldDataWalks[0] == null) {
+        alert("Please pull datasets before calculating");
+        return;
+    }
+
+    for (var i=0; i<30; i++) {
+        calculatedRuns.push((Number(fieldDataWalks[i]) + Number(fieldDataTotalBases[i])) /4.0);
+    }
+
+    $("#calculatedRuns").text("Calculated Runs: " + calculatedRuns.join());
+      
 }
 
 function post() {
@@ -68,14 +69,14 @@ function post() {
 
     if (calculatedRuns[0] == null) {
         alert("There are no calculated runs. Please press Calculate Runs before posting");
-    } else {
-
-        $.post(API_URL, upload);
-
-        clearData();
-
-        alert("Runs have been Posted");
+        return;
     }
+
+    $.post(API_URL, upload);
+
+    clearData();
+
+    alert("Runs have been Posted");
 }
 
 function clearData() {
