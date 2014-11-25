5360dfc489a72183fbec9731d88e7a52d164eedd
Tyler Puleo <tyler.puleo22@gmail.com>
2014-11-25 15:03:48 -0500

    Reformatted

--- HTMLandJS/SimplePost/Post.js
+++ HTMLandJS/SimplePost/Post.js
@@ -1,7 +1,7 @@
-//This script is hard coded for project 556. To change project you must change the 556 in the url and add the appropriate field numbers.
-function submitter()
-{
-    var API_URL = 'http://isenseproject.org/api/v1/projects/556/jsonDataUpload';
+"use strict";
+function submitter(){
+
+    var apiUrl = 'http://isenseproject.org/api/v1/projects/556/jsonDataUpload';
     var formData = [];
     var upload = {
         'email': 'j@j.j',
@@ -16,5 +16,5 @@ function submitter()
             '2643': [8]
         }
     }
-        $.post(API_URL, upload);
+        $.post(apiUrl, upload);
 }
