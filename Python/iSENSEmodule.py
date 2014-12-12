8f6d038b6363d374597a19499f7e08f01bcc6962
Tyler <tyler.puleo22@gmail.com>
2014-12-12 17:28:43 -0500

    Finished Get for python class

---
+++ Python/iSENSEmodule.py
@@ -0,0 +1,66 @@
+import requests
+
+baseUrl = 'http://rsense-dev.cs.uml.edu/api/v1/projects/';
+
+def projectGetRequest(projectID):
+    urlProject = baseUrl+projectID+'?recur=true';
+
+    data = requests.get(urlProject)
+
+    return data
+def getDatasetLocation(datasetName,parsedResponseProject):
+
+    j = 0
+
+
+    while j < parsedResponseProject.json()['dataSetCount']:
+
+
+        if parsedResponseProject.json()['dataSets'][j]['name'] == datasetName:
+            print 'heyyyyyyyyyyyyyyy'
+            datasetLocation = j
+            datasetID = parsedResponseProject.json()['dataSets'][j]['id']
+            return datasetLocation
+        j+=1
+
+    return 'Dataset not found'
+
+def getFieldID(fieldName,parsedResponseProject):
+
+    i = 0
+
+    while i < parsedResponseProject.json()['fieldCount']:
+
+        if parsedResponseProject.json()['fields'][i]['name'] == fieldName:
+            fieldID = parsedResponseProject.json()['fields'][i]['id']
+            return fieldID;
+        i += 1
+        
+    return "Field Not Found"
+    
+def getDatasetFieldData(projectID,datasetName,fieldName):
+
+    values = []
+    k = 0
+
+    parsedResponseProject = projectGetRequest(projectID)
+
+    datasetLocation = getDatasetLocation(datasetName,parsedResponseProject)
+
+    fieldID = getFieldID(fieldName,parsedResponseProject)
+
+    fieldID = str(fieldID)
+
+    while k < parsedResponseProject.json()['dataSets'][datasetLocation]['datapointCount']:
+
+        values.append(parsedResponseProject.json()['dataSets'][datasetLocation]['data'][k][fieldID])  
+        k += 1 
+    
+
+    return values
+
+
+
+
+
+
---
+++ Python/testClass.py
@@ -0,0 +1,11 @@
+
+
+import iSENSEmodule
+
+"""data = iSENSEmodule.projectGetRequest('106')
+
+print iSENSEmodule.getDatasetLocation('MLB Team Statistics 2013',data)
+
+print iSENSEmodule.getFieldID('Runs',data)"""
+
+print iSENSEmodule.getDatasetFieldData('106','MLB Team Statistics 2013','Runs')
