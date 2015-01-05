6728a2f482e40796ae92ca16ff6df583ddad6757
Tyler <tyler.puleo22@gmail.com>
2015-01-05 11:23:33 -0500

    newlines

--- Python/LibraryBaseballApp/BaseballApp.py
+++ Python/LibraryBaseballApp/BaseballApp.py
@@ -14,10 +14,9 @@ for i in range(0,30):
     total_walks.append(int(total_walks_strings[i]))
     total_bases.append(int(total_bases_strings[i]))
     calculated_runs.append((total_walks[i] + total_bases[i])/4.0)
+
 print total_walks
-
 print "\n"
-
 print "Walks: " , total_walks
 print "Total Bases: " , total_bases
 print "Calculated Runs: " , calculated_runs
