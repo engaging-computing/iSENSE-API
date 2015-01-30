f0da7a5caae2a7c28e4501d22bc138178dd3ee5f
Tyler Puleo <tyler.puleo22@gmail.com>
2015-01-30 09:34:12 -0500

    removed comments

--- HTMLandJS/class/getClass.js
+++ HTMLandJS/class/getClass.js
@@ -11,22 +11,6 @@ function get(){
 	fields = ['Runs','Hits','Walks'];
 
 	isense.appendToDataset(fields,'titlesldfijsleifj',data);
-/*
-	console.log(isense.getDatasetFieldData(106,"MLB Team Statistics 2013","Hits"));  // Project ID, Dataset Name, Field Name
-
-
-	data = [5,4,8,4,5,3,3,84,5484]
-
-
-
-	isense.postDataset(927,"Key","Dice Roll","TEST","Tyler",data);  // Project ID,Contributor Key,Title,Contributor Name,data as an array
-<<<<<<< HEAD
-	*/
-}
-
-
-
-=======
 
 	fieldNameArray = ['Walks','Home Runs'];
 
@@ -39,4 +23,4 @@ function get(){
 
 	isense.postMultipleDataset(106,'Key',fieldNameArray,'title','Tyler',ArrayofDataArrays)
 }
->>>>>>> d284520582fc9b92075f98da6b6a3cbfc90b2d5a
+
